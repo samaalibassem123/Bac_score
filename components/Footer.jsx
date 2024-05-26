46ce43b9234925073682b662fa15c4a0c524ad50
samaalibassem123 <83089177+samaalibassem123@users.noreply.github.com>
2024-05-26 22:26:18 +0100

    Add files via upload

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MdEmail } from "react-icons/md";
+import { FaDiscord } from "react-icons/fa6";
 export default function Footer() {
   return (
     <footer className="p-2 m-0 rounded-sm  bg-black text-white select-none">
@@ -7,7 +8,9 @@ export default function Footer() {
         Created By Bassem Samaali Contact:{" "}
         <span className="flex items-center gap-1 hover:bg-slate-100 hover:text-black cursor-text select-text">
           <MdEmail />
-          samaalibassem123@gmail.com
+          samaalibassem123@gmail.com |
+          <FaDiscord />
+          .besbes
         </span>
       </p>
     </footer>
