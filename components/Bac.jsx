219ba7bc6b3f48dbe591682d3f4fb911b4c6f219
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2024-05-29 22:07:47 +0100

    Add files via upload

--- components/Bac.jsx
+++ components/Bac.jsx
@@ -9,9 +9,15 @@ export default function Bac(props) {
   return (
     <div className="bg-white p-5 flex gap-5  flex-col sm:w-fit sm:h-[400px]   items-center justify-center rounded-lg ">
       <h1 className="text-2xl font-bold m-2">{bacname}</h1>
-      <img src={linkimg} alt="refrech the page !" className="w-20 h-20 " />
+      <img
+        src={linkimg}
+        alt="refrech the page !"
+        className="w-20 h-20 "
+        title={bacname}
+      />
       <Link
         href={linkBac}
+        title={bacname}
         className="sm:w-auto w-full text-center  animate-bounce hover:animate-none rounded-lg border m-5 p-3 px-10  border-black text-lg hover:bg-black hover:text-white transition-all duration-75 hover:scale-110"
       >
         Start
--- components/CorP.jsx
+++ components/CorP.jsx
@@ -9,12 +9,14 @@ export default function CorP(props) {
     <div className="flex flex-col gap-24 font-ft select-none">
       <Link
         href={BacLinkP}
+        title={BacName}
         className="  text-3xl sm:bg-white rounded-lg sm:text-black bg-black text-white border border-black p-5 sm:px-[100px]  tracking-wide text-center  outline-none hover:scale-105 transition-all delay-75  sm:hover:text-white sm:hover:bg-black"
       >
         Session Principale
       </Link>
       <Link
         href={BacLinkC}
+        title={BacName}
         className="  text-3xl sm:bg-white rounded-lg sm:text-black bg-black text-white border border-black p-5 sm:px-[100px]  tracking-wide text-center   outline-none hover:scale-105 transition-all delay-75  sm:hover:text-white sm:hover:bg-black"
       >
         Session Controle
