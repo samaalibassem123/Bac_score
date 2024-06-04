955a8deb3a61c03804f30e56e8c260ae7ac3bd7a
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2024-06-04 15:37:46 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="google-adsense-account" content="ca-pub-2432091582720443">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link
